a7ecb6fcf126755e537a6a86883c6c5f73f986ac
Seyedmohammad Khataeipour <mohammadkhataei@icloud.com>
2024-10-09 21:01:07 +0200

    Changes in CallNextTicket API

--- server/controllers/ticketController.mjs
+++ server/controllers/ticketController.mjs
@@ -3,9 +3,11 @@ import { openDatabase } from '../config/db.mjs';
 
 // API to call the next ticket
 export const callNextTicket = (req, res) => {
+  const serviceId = req.query.serviceId;
   openDatabase().then((db) => {
-    // Find the first waiting ticket (not served)
-    db.get(`SELECT * FROM tickets WHERE served = 0 ORDER BY day ASC, idTicket ASC LIMIT 1`, (err, ticket) => {
+    const query = `SELECT * FROM tickets WHERE served = 0` + (serviceId ? ` AND service = ?` : '') + ` ORDER BY day ASC, idTicket ASC LIMIT 1`;
+    
+    db.get(query, serviceId ? [serviceId] : [], (err, ticket) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ message: 'Error retrieving the ticket' });
@@ -14,22 +16,14 @@ export const callNextTicket = (req, res) => {
       if (!ticket) {
         return res.status(404).json({ message: 'No waiting tickets' });
       }
-
-      // Mark the ticket as served
-      db.run(`UPDATE tickets SET served = 1 WHERE idTicket = ? AND day = ?`, [ticket.idTicket, ticket.day], function (err) {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ message: 'Error updating the ticket' });
-        }
-
-        res.status(200).json({ message: 'Ticket called', ticket });
-      });
+      markTicketAsCalled(db, ticket, res);
     });
   }).catch((error) => {
     console.error(error);
     res.status(500).json({ message: 'Error opening the database' });
   });
 };
+
 
 // API to insert a new ticket in DB
 export const createTicket = (req, res) => {
