e59a49cbf6d251093e87111b982d5444973c1b79
Giada Socorro s334048 <s334048@studenti.polito.it>
2024-10-10 17:14:00 +0200

    createTicket and getTicket api client side

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -20,3 +20,43 @@ export const callNextTicket = async () => {
     throw error;
   }
 };
+
+export const createTicket = async (idTicket, day, service, counter, served) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/tickets/create`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      //credentials: 'include',
+      body: JSON.stringify({idTicket: idTicket, day: day, service: service, counter: counter, served: served})
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data; 
+  } catch (err) {
+    console.error('Failed to create a new ticket:', err);
+    throw err;
+  }
+};
+
+export const getTicket = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/tickets/get`);
+    //, {credentials: 'include'});
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data; 
+  } catch (err) {
+    console.error('Failed to get the ticket:', err);
+    throw err;
+  }
+};
