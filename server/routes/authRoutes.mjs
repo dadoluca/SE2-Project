6192c77da9f77c4baca7a24668b11b8c157baeee
Seyedmohammad Khataeipour <mohammadkhataei@icloud.com>
2024-10-11 17:45:31 +0200

    implemented Cors, login and logout APIs and Passport for authentication

---
+++ server/routes/authRoutes.mjs
@@ -0,0 +1,39 @@
+// src/routes/authRoutes.mjs
+
+import express from 'express';
+import passport from '../middlewares/passportConfig.mjs'; // Adjust the path according to your project structure
+
+const router = express.Router();
+
+// Login route
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return res.status(500).json({ error: 'An error occurred during authentication.' });
+    }
+    if (!user) {
+      return res.status(401).json({ error: info.message }); // Missing credentials
+    }
+
+    // Log in the user
+    req.logIn(user, (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to log in user.' });
+      }
+      return res.status(200).json({ message: 'Logged in successfully', user }); // Successful login
+    });
+  })(req, res, next);
+});
+
+// Logout route
+router.post('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).json({ error: 'An error occurred during logout.' });
+    }
+    return res.status(200).json({ message: 'Logged out successfully' }); // Successful logout
+  });
+});
+
+// Export the router as default
+export default router;
