f704c8e13b771909ba28e6f0b9dcfe8579c0ccc3
Seyedmohammad Khataeipour <mohammadkhataei@icloud.com>
2024-10-16 19:35:46 +0200

    implement test units for serviceController

--- server/config/db.mjs
+++ server/config/db.mjs
@@ -2,7 +2,7 @@ import sqlite3 from 'sqlite3';
 
 // Function to open a SQLite database
 export const openDatabase = () => {
-    return new sqlite3.Database('queue.sqlite', (err) => {
+    return new sqlite3.Database('queue.sqlite',sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             console.error('Error opening database ' + err.message);
         }
